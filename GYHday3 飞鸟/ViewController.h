b564175072210bd0188a25e74a098b09606af64b
goyohol <qianfeng@chengduqianfengdeMac-mini.local>
2016-03-29 17:16:37 +0800

    flyBird
    a bird fly forever,Cuz UIImageView’s animation and it’s frame.origin
    move~

---
+++ GYHday3 飞鸟/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GYHday3 飞鸟
+//
+//  Created by goyohol on 16/2/17.
+//  Copyright (c) 2016年 goyohol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
